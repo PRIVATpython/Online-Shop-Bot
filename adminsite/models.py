300b3d4322ebe04d15102a292f3bd2bc4fdd0566
Your Name <you@example.com>
2021-09-16 00:35:12 +0500

    v 2.1

--- adminsite/models.py
+++ adminsite/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
+
 class Users(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=25)
     tel = models.CharField(max_length=20)
-
-    tel = models.CharField(max_length=20)
+    language = models.CharField(max_length=20)
