d191df8cec4d9fb7e3a2dba1792b5758cd9eaf81
Your Name <you@example.com>
2021-09-30 19:42:07 +0500

    models v1

--- adminsite/models.py
+++ adminsite/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 
-class Users(models.Model):
-    id = models.AutoField(primary_key=True)
+class User(models.Model):
+
     name = models.CharField(max_length=25)
     tel = models.CharField(max_length=20)
     language = models.CharField(max_length=20)
+
+
+class Order(models.Model):
+    user_fk_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_order")
